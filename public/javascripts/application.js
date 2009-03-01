6181a604d9a5ff67f24b588b355012c2e1f3d59e
Kim Ki-yong <dragonkun@gmail.com>
2009-03-01 18:23:29 +0900

    회원가입 폼 Ajax로 validation 하기

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,23 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+//
+
+$(function() {
+	$("#user_login").change(function() {
+		$.get('/users/login_check', {'user_login':$("#user_login").val()}, function(result) {
+			$("#login_check").html(result);
+		});
+	});
+
+	$("#user_email").change(function() {
+		$.get('/users/email_check', {'user_email':$("#user_email").val()}, function(result) {
+			$("#email_check").html(result);
+		});
+	});
+
+	$("#user_password_confirmation").change(function() {
+		$.get('/users/password_check', {'user_password':$("#user_password").val(), 'user_password_confirmation':$("#user_password_confirmation").val()}, function(result) {
+			$("#password_check").html(result);
+		});
+	});
+});
